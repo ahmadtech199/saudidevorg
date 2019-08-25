c07d2ce61d87d423a9598dd9fe0fbc8ebc2fed35
ahmad-1991 <abn1991@gmail.com>
2019-08-25 20:57:55 +0300

    Update Contents.swift

--- Quiz-answer-w1.playground/Contents.swift
+++ Quiz-answer-w1.playground/Contents.swift
@@ -1,12 +1,11 @@
-import UIKit
 
 
 // q1 & answer
 var Salary =  5000;
-var expensesMoneyInmonth = Salary - 1000 - 700 - 1300
-var SavingMoneyInmonth = Salary  - expensesMoneyInmonth
-var allMoneyInYear = Salary * 12
-var expensesMoneyInYear = expensesMoneyInmonth * 12
+var expensesMoneyInmonth = Salary - 1000 - 700 - 1300  //Output:  2000
+var SavingMoneyInmonth = Salary  - expensesMoneyInmonth //Output:  3000
+var allMoneyInYear = Salary * 12 //Output:  60000
+var expensesMoneyInYear = expensesMoneyInmonth * 12 //Output:  24000
 
 
 // q2 & answer
@@ -18,15 +17,19 @@ var iLoveSwift:Bool = true
 
 // q3 & answer
 var agee:Int = 12
-let allowedToEnter = agee >= 18
+let allowedToEnter = agee >= 18   //Output: false
 
 
 // q4 & answer
-10 != 5 + 5
+10 != 5 + 5  //Output: false
 
 
 // q5 & answer
 let audienceRating = 85
 let criticsRating = 75
 let recommendedByFriend = true
-let goWatchMovie = (audienceRating > 90 && criticsRating > 80) || recommendedByFriend
+let goWatchMovie = (audienceRating > 90 && criticsRating > 80) || recommendedByFriend //Output: true
+
+
+
+
