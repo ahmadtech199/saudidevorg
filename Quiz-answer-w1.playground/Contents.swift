8c7933cec7bb553bdd827b579f41470aae0c2ef1
ahmad-1991 <abn1991@gmail.com>
2019-08-25 20:49:51 +0300

    add file answers

---
+++ Quiz-answer-w1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+
+// q1 & answer
+var Salary =  5000;
+var expensesMoneyInmonth = Salary - 1000 - 700 - 1300
+var SavingMoneyInmonth = Salary  - expensesMoneyInmonth
+var allMoneyInYear = Salary * 12
+var expensesMoneyInYear = expensesMoneyInmonth * 12
+
+
+// q2 & answer
+var sign:String = "Smoking is not allowed"
+var age:Int = 22
+var speed:Float = 54.7
+var iLoveSwift:Bool = true
+
+
+// q3 & answer
+var agee:Int = 12
+let allowedToEnter = agee >= 18
+
+
+// q4 & answer
+10 != 5 + 5
+
+
+// q5 & answer
+let audienceRating = 85
+let criticsRating = 75
+let recommendedByFriend = true
+let goWatchMovie = (audienceRating > 90 && criticsRating > 80) || recommendedByFriend
